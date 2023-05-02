d5691a7d9623e4aa97dd0d2cfe67933b0a242ddd
Jonas <jonasmooney2@gmail.com>
2023-05-02 07:28:39 -0600

    Progress on pagination plans

--- frontend/src/components/DevBox.tsx
+++ frontend/src/components/DevBox.tsx
@@ -22,7 +22,7 @@ const DevBox = (prop: HandleLoadingFunc) => {
     const [devs, setDevs] = useState([])
     const [pageTotal, setPageTotal] = useState(0)
     //   const [pageNumber, setPageNumber] = useState(1)
-    const [paginationNumbersDisplayed, setPaginationNumbersDisplayed] = useState([])
+    const [pageNumbersDisplayed, setPageNumbersDisplayed] = useState([1, 6])
 
   
     const [queryObject, setQueryObject] = useState({
@@ -32,7 +32,8 @@ const DevBox = (prop: HandleLoadingFunc) => {
     })
     
     const pages = new Array(pageTotal).fill(null).map((v, i) => i)
-    const pagesSlice = pages.slice(queryObject.page, queryObject.page + 5)
+    // const pagesSlice = pages.slice(queryObject.page, queryObject.page + 5)
+    const pagesSlice = pages.slice(pageNumbersDisplayed[0], pageNumbersDisplayed[1])
 
     // pagesSlice needs to be a slice of five at a time out of the page total.
     // Instead of slicing on the queryObject.page, this should maintain its own state.
@@ -79,6 +80,16 @@ const DevBox = (prop: HandleLoadingFunc) => {
         })
     }
 
+    const handlePageUpOrDown = (arg: boolean) => {
+        if (arg === true) {
+            console.log(pageNumbersDisplayed)
+            const pageIncrement = pageNumbersDisplayed.map((s) => ++s)
+            // As trippy as this may seem, the prefix operator is needed.
+            // If a postfix operator was used, the map function would return 's' before the increment took place.
+            console.log(pageIncrement)
+        }
+    }
+
     const devList = devs.map(({first_name, last_name, email, hourly_rate, star_rating}) =>
             <div key={uuidv4()} className='devBox'>
                 <div className='namef'>
@@ -101,13 +112,13 @@ const DevBox = (prop: HandleLoadingFunc) => {
             <div className="allDevsContainer">
                 {devList}
             </div>
-            <button>less than</button>
+            <button onClick={() => handlePageUpOrDown(false)}>less than</button>
             {/* less than and greater than always shown to toggle not to the next group of five but */}
             {/* to the very next page. */}
             {pagesSlice.map((pageIndex) => (
                 <button key={pageIndex} onClick={() => handlePagination(pageIndex)}>{pageIndex}</button>
             ))}
-            <button>greater than</button>
+            <button onClick={() => handlePageUpOrDown(true)}>greater than</button>
         </div>
     </>
     )
